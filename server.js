3bcd3cff21200f0d7a2fc00adf081fde66b014c6
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-03-25 14:34:34 +0200

    connect to db

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const connectDb=require('./config/db')
 
 const app = express();
+//connect to DB
+connectDb()
 app.get("/", (req, res) => {
   res.json({ msg: "Contact keeper API" });
 });
