636153dbf3c4d7e6e36551d5f097248cc46ea31a
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-03-28 17:48:16 +0200

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const connectDb=require('./config/db')
+const path = require("path");
+const connectDb = require("./config/db");
 
 const app = express();
 //connect to DB
-connectDb()
+connectDb();
 //Init middleware
 //re body
-app.use(express.json({extended:false}))
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) => {
   res.json({ msg: "Contact keeper API" });
 });
@@ -15,6 +16,15 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
